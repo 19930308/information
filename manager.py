fd00b2a50b937aae439b75f76ce0292284c1109f
huwei <yue@163.com>
2018-06-19 12:58:28 +0800

    项目配置&初始化mysql数据库

--- manager.py
+++ manager.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+
+class Config(object):
+    """项目文件配置"""
+    DEBUG = True
+    # 连接mysql数据库
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:6379/news"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# 创建flask实例对象
 app = Flask(__name__)
-app.debug = True
+# 加载配置
+app.config.from_object(Config)
+# 初始化mysql数据库
+db = SQLAlchemy(app)
 
 
 @app.route('/')
