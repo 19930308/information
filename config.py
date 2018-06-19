78a962f411aa05e386395d9d578b9ef4b7a31509
huwei <yue@163.com>
2018-06-19 19:22:42 +0800

    抽取配置文件

---
+++ config.py
@@ -0,0 +1,25 @@
+from redis import StrictRedis
+
+
+class Config(object):
+    """项目文件配置"""
+    DEBUG = True
+    # 设置密匙
+    SECRET_KEY = "JF0dbTOmNuIiP7L0SJxdpHi7YB1Dr7mU2AshyKt9tGFzfSiXNva6DOhu237ZxxGX"
+
+    # 连接mysql数据库
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:6379/news"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 设置session保存在redis数据库
+    SESSION_TYPE="redis"
+    # 开启session签名
+    SESSION_USE_SINGER=True
+    # 指定session保存的数据库
+    HOST="127.0.0.1"
+    PORT="6379"
+    SESSION_REDIS=StrictRedis(host=HOST,port=PORT)
+    # 设置session为需要过期
+    SESSION_PERMANATE=False
+    # 设置过期时间
+    SESSION_LIFETIME=86400*2
